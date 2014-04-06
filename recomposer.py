bec8ab908009a5d111ef3d3263469f08e48e846d
Roland Crosby <roland@rolandcrosby.com>
2014-04-05 17:56:35 -0700

    first commit

---
+++ recomposer.py
@@ -0,0 +1,24 @@
+import unicodedata
+import re
+import string
+
+class Recomposer(object):
+	def __init__(self):
+		self.make_substitutions()
+
+	def make_substitutions(self):
+		self.substitutions = {}
+		for i in range(1, 65535):
+			char = unichr(i)
+			decomposed = unicodedata.normalize('NFKD', char)
+			if len(decomposed) > 1:
+				result = re.match(r'^[A-z]+$', decomposed)
+				if result:
+					self.substitutions[decomposed] = char
+
+	def process(self, instr):
+		outstr = unicode(instr)
+		for sub in sorted(self.substitutions.keys(), lambda x, y: len(y) - len(x)):
+			if sub in outstr:
+				outstr = string.replace(outstr, sub, self.substitutions[sub])
+		return outstr
